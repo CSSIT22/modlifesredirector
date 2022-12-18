21410c16aa9251df423cac49d990213808fb4762
I_AM_TINE <sutichok46@gmail.com>
2022-12-19 01:55:23 +0700

    [HOTFIX] redirect url

--- src/pathredirect.rs
+++ src/pathredirect.rs
@@ -2,7 +2,7 @@ use rocket::response::Redirect;
 
 #[get("/<path>")]
 pub fn redx(path: &str) -> Redirect{
-    let mut origin = "https://ss.modlifes.me/shortlink/redirect?shorten=".to_owned();
+    let mut origin = "https://staging-api.modlifes.me/shortlink/redirect?shorten=".to_owned();
     origin.push_str(path);
     Redirect::to(origin)
 }
