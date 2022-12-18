714e5299b07ce15c2095f54afa7964a953723fe7
I_AM_TINE <sutichok46@gmail.com>
2022-12-19 01:53:06 +0700

    [FIX] Redirect url

--- src/pathredirect.rs
+++ src/pathredirect.rs
@@ -2,7 +2,7 @@ use rocket::response::Redirect;
 
 #[get("/<path>")]
 pub fn redx(path: &str) -> Redirect{
-    let mut origin = "https://s.thistine.com/".to_owned();
+    let mut origin = "https://s.modlifes.me/shortlink/redirect?shorten=".to_owned();
     origin.push_str(path);
     Redirect::to(origin)
 }
